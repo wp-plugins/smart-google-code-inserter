3330257e9e3556edb2fe9a85da660d1b4810c033
Oturia <Oturia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-01 15:52:33 +0000

    git-svn-id: https://plugins.svn.wordpress.org/smart-google-code-inserter/trunk@606592 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smartgooglecode.php
+++ smartgooglecode.php
@@ -1,13 +1,13 @@
 <?php 
 #     /* 
-#     Plugin Name: Smart Google Code Inserter
+#     Plugin Name: Smart Google Analytics Code
 #     Plugin URI: http://oturia.com/
-#     Description: Easily add your Google Analytics code and Webmaster Tools verification into the header with this plugin.
+#     Description: Smart Google Analytics, Webmaster Tools and AdWords Code
 #     Author: Jason Whitaker
-#     Version: 1.0 
+#     Version: 2.0 
 #     Author URI: http://oturia.com/
 #     */  
-
+ ini_set( "short_open_tag", 1 ); 
 
 if( !class_exists('SmartGoogleCode') )
 {
@@ -20,6 +20,8 @@ if( !class_exists('SmartGoogleCode') )
 				add_action( 'admin_menu', array($this, 'my_plugin_menu') );
 				
 				add_action('wp_head', array($this,'smart_webmaster_head'));
+				
+				add_action('wp_footer', array($this,'smart_google_pcc_code'));
 				
 			# Update General Settings
 				if( $_POST['action'] == 'savegooglecode' )
@@ -46,7 +48,30 @@ $Varsgcwebtools  = get_option( 'sgcwebtools' );
 
 $Varsgcgoogleanalytic  = get_option( 'sgcgoogleanalytic' );
 
+
+$ppccode = get_option( 'ppccode' );
+$ppccap = get_option( 'ppccap' );
+$ppcpageid = get_option( 'ppcpageid' );
+
+if( $ppccap == "") {
+	$ppccap="ex: mywplead";
+}
+
 ?>
+
+<script type="text/javascript" language="javascript">
+function CheckPPC(){
+
+	if(document.getElementById("ppccode").value != "" ) {
+		
+		if(document.getElementById("ppcpageid").value == "-1" ){
+			alert("Please Select Page");
+			return false;
+		}
+	}
+}
+</script>
+
 
 <div class="wrap">
   <h2>Smart Google Code</h2>
@@ -58,7 +83,7 @@ $Varsgcgoogleanalytic  = get_option( 'sgcgoogleanalytic' );
   </div>
   <br />
   <?php } ?>
-  <form name="generalform" id="generalform" method="post" action="" >
+  <form name="generalform" id="generalform" method="post" action="" onsubmit="return CheckPPC();" >
     <div class="metabox-holder" style="width: 800px;">
       <div class="postbox">
         <h3>Google Analytics Settings</h3>
@@ -93,6 +118,46 @@ $Varsgcgoogleanalytic  = get_option( 'sgcgoogleanalytic' );
     </div>
     <!-- end .metabox-holder -->
     
+    
+    <div class="metabox-holder" style="width: 800px;">
+      <div class="postbox">
+        <h3>Conversion Code Settings</h3>
+        <div>
+          <table class="form-table">
+            <tbody>
+              <tr valign="top" class="alternate">
+                <th style="width:30%;" scope="row">
+                <label style="padding: 0 0 15px 0;">Caption</label><br />
+
+                <input name="ppccap" type="text" id="ppccap" value="<?php echo stripslashes($ppccap); ?>" />
+                
+                <br />
+				<br />
+				<br />
+                <label style="padding: 0 0 16px 0;">Please Select Page</label><br />
+
+          
+                                
+                <select name="ppcpageid" id="ppcpageid" style="width: 150px;">
+                <option value="-1">Select Page</option>
+                <?php $pages = get_pages(); 
+					  foreach ( $pages as $page ) {
+				?> 
+                <option value="<?php echo $page->ID ?>" <?php if($ppcpageid == $page->ID ) {?> selected="selected" <?php } ?> ><?php echo $page->post_title; ?></option>
+                
+				<?php } ?>                 
+                
+                </select>
+                
+                </th>
+                <td style="width:90%;"><textarea rows="7" cols="90" id="ppccode" name="ppccode"><?php echo stripslashes($ppccode); ?></textarea></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    
     <div class="submit">
       <input type="submit" name="button" id="button" class="button-primary" value="<?php echo _e("Save Changes"); ?>" />
     </div>
@@ -108,6 +173,12 @@ $Varsgcgoogleanalytic  = get_option( 'sgcgoogleanalytic' );
 			update_option( 'sgcwebtools', $_POST["sgcwebtools"] );
 	
 			update_option( 'sgcgoogleanalytic', $_POST["sgcgoogleanalytic"] );
+
+			update_option( 'ppccode', $_POST["ppccode"] );
+			
+			update_option( 'ppccap', $_POST["ppccap"] );
+			
+			update_option( 'ppcpageid', $_POST["ppcpageid"] );
 
 			$_POST['notice'] = __('Settings Saved');			
 		
@@ -128,11 +199,28 @@ $Varsgcgoogleanalytic  = get_option( 'sgcgoogleanalytic' );
 		
 	}
 
+	function smart_google_pcc_code()
+	{
+		global $post;
+		$pstId = $post->ID;
+
+		$ppccode = get_option( 'ppccode' );
+		$ppcpageid = get_option( 'ppcpageid' );
+		
+		if($ppcpageid == $pstId ) {
+			echo stripslashes($ppccode);
+		}
+		
+	}
+
 
 	function  InstallSmartDefaultValues() {
 
 		add_option("sgcwebtools", "");
 		add_option("sgcgoogleanalytic", "");
+		add_option("ppccode", "");
+		add_option("ppccap", "");
+		add_option("ppcpageid", "");				
 
 	}
 
@@ -140,7 +228,7 @@ $Varsgcgoogleanalytic  = get_option( 'sgcgoogleanalytic' );
 	}
 } 
 
-if( class_exists('SmartGoogleCode') )
+if( class_exists('SmartGoogleCode') ) {
 	$objSmartGoogleCode = new SmartGoogleCode();
-
+}
 ?>
